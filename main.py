8c467786b3916e46af94473bb90be9004bc7644a
ADIRU142 <adilrocks142@gmail.com>
2025-03-23 13:08:44 +0530

    initial commit

---
+++ main.py
@@ -0,0 +1,63 @@
+import speech_recognition as sr
+import webbrowser
+import pyttsx3
+import musicLibrary 
+import requests
+recognizer = sr.Recognizer()
+engine = pyttsx3.init()
+newsapi ="9aa0f026af314bfdadcdf6a9ed8ef37b"
+
+
+
+def speak(text):
+    engine.say(text)
+    engine.runAndWait()
+
+def processCommand(c):
+    if "open google" in c.lower():
+        webbrowser.open("https://google.com")
+    elif "open facebook" in c.lower():
+        webbrowser.open("https://facebook.com")
+    elif "open youtube" in c.lower():
+        webbrowser.open("https://youtube.com")
+    elif c.lower().startswith("play"):
+        song = c.lower().split(" ")[1]
+        link = musicLibrary.music[song]
+        webbrowser.open(link)
+
+
+    '''elif "okay" in c.lower():
+        r = requests.get(f"https://newsapi.org/v2/top-headlines?country=us&apiKey=apiKey
+            data = r.json()
+
+            articles = data.get('articles',[])
+
+            for article in articles:
+                speak(article['title'])'''
+    
+                  
+
+if __name__ == '__main__':
+    speak("I am On...")
+    while True:
+        
+        r = sr.Recognizer()
+        
+        print("recognizing...")
+
+        try:
+            with sr.Microphone() as source:
+                print("Listening...!")
+                audio = r.listen(source, timeout=2, phrase_time_limit=1)
+            word = r.recognize_google(audio)
+            if(word.lower() == "jarvis"):
+                speak("Yes")
+                with sr.Microphone() as source:
+                    print("Jarvis Active...")
+                    audio = r.listen(source)
+                    command = r.recognize_google(audio)
+
+                    processCommand(command)
+            
+        except Exception as e:
+            print("Error; {0}".format(e))
---
+++ musicLibrary.py
@@ -0,0 +1,4 @@
+music = {
+    "peace": "https://www.youtube.com/watch?v=UeSDvg5xsWI",
+    "harry": "https://youtu.be/tSn3U0ctmOs?si=yTfXbyfo5PzwFo7k"
+}
